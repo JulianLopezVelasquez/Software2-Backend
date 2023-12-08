32c829239aa0947266ad54c7b0d70774412de1d5
JulianLopezVelasquez <102310408+JulianLopezVelasquez@users.noreply.github.com>
2023-12-07 23:08:50 -0500

    update routes

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/plantilla')    
+
+@section('tituloPagina','Crud Usuarios')
+
+@section('contenido')
+    <div class="ro">
+        <h1>hola usando blade out</h1>
+    </div>
+@endsection
---
+++ storage/framework/views/5c37285fce103bd31703e646d35e1d89.php
@@ -0,0 +1,10 @@
+    
+
+<?php $__env->startSection('tituloPagina','Crud Usuarios'); ?>
+
+<?php $__env->startSection('contenido'); ?>
+    <div class="ro">
+        <h1>hola usando blade out</h1>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout/plantilla', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Manuel Muñoz\Documents\Code\ProyectoCarros\JuegoCarros\resources\views/welcome.blade.php ENDPATH**/ ?>
